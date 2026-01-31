fc09d4da77935aca55b4704adbc067b04851104d
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2026-01-31 15:41:55 +0530

    first commit

--- Roger AI/ContentView.swift
+++ Roger AI/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("What is your name.")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding()
     }
 }
 
